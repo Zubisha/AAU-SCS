6f5f43942ab6113e69d524411b86a3ea2d0b708d
Zubisha <zubisha4321@gmail.com>
2023-05-13 12:59:46 +0300

    build update

--- src/Student Pages/SideBar.js
+++ src/Student Pages/SideBar.js
@@ -12,8 +12,8 @@ export default props => {
       <div className='menu-list'>
         <div className="menu1"><img id="q" src={materialsymbolshomeoutlinerounded} style={{paddingRight:'5px'}}/><span><Link to="/HomeSTUD" id="p" style={{textDecoration:'none',color:'white',fontWeight:'bold'}}>Home</Link></span></div>
         <div className="menu1"><img id="q" src={Vector1} style={{paddingRight:'5px'}}/><span id="p" style={{textDecoration:'none',color:'white',fontWeight:'bold', width:'max-content'}}>Change Password</span></div>
-        <div className="menu1"><img id="q" src={materialsymbolshelp} style={{paddingRight:'10px'}}/><span id="p" style={{textDecoration:'none',color:'white',fontWeight:'bold'}}>Request Clearance</span></div>
-        <div className="menu1"><img id="q" src={materialsymbolshelp} style={{paddingRight:'10px'}}/><span id="p" style={{textDecoration:'none',color:'white',fontWeight:'bold'}}>Notifications</span></div>
+        <div className="menu1"><img id="q" src={materialsymbolshelp} style={{paddingRight:'10px'}}/><span><Link to="/RequestClearanceSTUD" id="p" style={{textDecoration:'none',color:'white',fontWeight:'bold'}}>Request Clearance</Link></span></div>
+        <div className="menu1"><img id="q" src={materialsymbolshelp} style={{paddingRight:'10px'}}/><span><Link to="/ClearanceStatusSTUD" id="p" style={{textDecoration:'none',color:'white',fontWeight:'bold'}}>Notifications</Link></span></div>
         <div className="menu1"><img id="q" src={materialsymbolshelp} style={{paddingRight:'10px'}}/><span id="p" style={{textDecoration:'none',color:'white',fontWeight:'bold'}}>Help</span></div>
         <div className="menu1"><img id="q" src={uil_signout} style={{paddingRight:'5px'}}/><span><Link to="/SigninSTUD" id="p" style={{textDecoration:'none',color:'white',fontWeight:'bold'}}>Signout</Link></span></div>
       </div>
@@ -22,3 +22,4 @@ export default props => {
 };
 
 
+<Link to="/RequestClearanceSTUD" style={{textDecoration:'none'}}> <span className="labelSTUD" style={{color:'white'}}> Request Clearance</span></Link> 
